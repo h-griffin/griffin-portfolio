9616d7fe9e7e336d9fe0755755da08797b8c66b0
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-12-04 15:58:18 -0800

    css not working

---
+++ src/containers/Body/Body.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import classes from './Body.css';
+
+
+class BurgerBuilder extends Component{
+     
+    render(){
+
+        return (
+            <div className={classes.Body}>
+                <h1>Griffin Portfolio</h1>
+            </div>
+
+            // background
+            // body 
+            //    nav
+            //    
+
+        );
+    }
+}
+
+export default BurgerBuilder;

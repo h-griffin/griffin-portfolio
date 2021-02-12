7c6ddff63ac2ef2cb877e050e8ac8fd7ec0e5f0e
Griffin <60614863+h-griffin@users.noreply.github.com>
2021-02-11 23:57:31 -0800

    react font awesome

--- src/components/Navigation/NavigationItems.js
+++ src/components/Navigation/NavigationItems.js
@@ -4,20 +4,28 @@ import classes from './NavigationItems.css'
 import NavigationItem from './NavigationItems/NavigationItem';
 
 import { withRouter } from 'react-router';
+import Aux from '../../hoc/Aux/Aux';
+
 
 
 const NavigationItems = () => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link='/' >Home</NavigationItem>
-        <NavigationItem link='/code' >Code</NavigationItem>
-        <NavigationItem link='/certificates'>Certificates</NavigationItem>
-        <NavigationItem link='/art'>Art</NavigationItem>
-        <NavigationItem link='/contact'>Contact</NavigationItem>
-
+    <Aux>
         
-    </ul>
+        <ul className={classes.NavigationItems}>
+            <NavigationItem link='/' iconClass="icon solid fa-home">Home</NavigationItem>
+            <NavigationItem link='/code' >Code</NavigationItem>
+            <NavigationItem link='/certificates'>Certificates</NavigationItem>
+            <NavigationItem link='/art'>Art</NavigationItem>
+            <NavigationItem link='/contact'>Contact</NavigationItem>
+
+            
+        </ul>
+    </Aux>
+    
 
 
 );
 
 export default withRouter(NavigationItems);
+
+// class="icon solid fa-home"
